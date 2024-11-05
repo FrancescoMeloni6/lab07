ac46e238dde3faf0ab257d64ab4891974f43b591
Francesco Meloni - francesco.meloni6@studio.unibo.it <francesco.meloni6@studio.unibo.it>
2024-11-05 16:40:40 +0100

    Done

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -10,10 +10,22 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
 
     private T[] array;
     private int currentElement;
+    private Predicate<T> predicate;
 
-    public IterableWithPolicyImpl(T[] array){
+    public IterableWithPolicyImpl(T[] array, Predicate<T> predicate){
         this.array = array;
         currentElement = 0;
+        setIterationPolicy(predicate);
+    }
+    
+    public IterableWithPolicyImpl(T[] array){
+        this(array, new Predicate<>() {
+                @Override
+                public boolean test(T elem) {
+                    return true;
+                }
+            }
+        );
     }
 
     @Override
@@ -23,19 +35,23 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
 
     @Override
     public void setIterationPolicy(Predicate<T> filter) {
-        
+        this.predicate = filter;
     }
 
     public class IteratorImpl implements Iterator<T>{ 
 
         @Override
         public boolean hasNext() {
+            if (currentElement < array.length && !predicate.test(array[currentElement])) {
+                currentElement++;
+                hasNext();
+            }
             return currentElement < array.length;
         }
 
         @Override
         public T next() {
-            if (!hasNext()) {
+            if (currentElement >= array.length) {
                 throw new NoSuchElementException();
             }
             
